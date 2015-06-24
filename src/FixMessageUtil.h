be21c7da2fe930bfcd8b9aed9a49a577d68ebb78
Kermen Deol <kermen.deol@electronifie.com>
2015-06-24 16:47:07 -0400

    Revert "Merge pull request #18 from electronifie/feature/recursive-groups"
    This reverts commit 22795e5ca2fe214ba0a748361d1c307354841ffa, reversing
    changes made to 532e31932361f26f7ebe10e3532ca9b6a128b6b3.

--- src/FixMessageUtil.h
+++ src/FixMessageUtil.h
@@ -23,246 +23,124 @@ public:
 	FixMessageUtil();
 	virtual ~FixMessageUtil();
 
-	static void addFixHeader(FIX::Message* message, Local<v8::Object> msg) {
-		
+	static void js2Fix(FIX::Message* message, Local<v8::Object> msg) {
+
 		Local<v8::Object> header = Local<v8::Object>::Cast(msg->Get(NanNew<String>("header")));
+		Local<v8::Object> tags = Local<v8::Object>::Cast(msg->Get(NanNew<String>("tags")));
+
 		FIX::Header &msgHeader = message->getHeader();
+		FIX::Trailer &msgTrailer = message->getTrailer();
+
 		Local<v8::Array> headerTags = header->GetPropertyNames();
-		
 		for(int i=0; i < (int)headerTags->Length(); i++) {
 			String::Utf8Value value(header->Get(headerTags->Get(i))->ToString());
+
 			msgHeader.setField(
 					headerTags->Get(i)->Int32Value(),
 					std::string(*value)
 			);
 		}
-	}
 
-	static void addFixTags(FIX::FieldMap* map, Local<v8::Object> msg) {
+		Local<v8::Array> msgTags = tags->GetPropertyNames();
+		for(int i=0; i < (int)msgTags->Length(); i++) {
+			String::Utf8Value value(tags->Get(msgTags->Get(i))->ToString());
 
-		Local<v8::String> tagsKey = NanNew<v8::String>("tags");
-		
-		if(msg->Has(tagsKey)) {
-
-			Local<v8::Object> tags = Local<v8::Object>::Cast(msg->Get(tagsKey));
-
-			Local<v8::Array> msgTags = tags->GetPropertyNames();
-			
-			for(int i=0; i < (int)msgTags->Length(); i++) {
-				String::Utf8Value value(tags->Get(msgTags->Get(i))->ToString());
-
-				
-				map->setField(
-						msgTags->Get(i)->Int32Value(),
-						std::string(*value)
-				);
-			}
-
+			message->setField(
+					msgTags->Get(i)->Int32Value(),
+					std::string(*value)
+			);
 		}
 
-	}
-
-	static void addFixGroups(FIX::FieldMap* map, Local<v8::Object> msg) {
 		Local<v8::String> groupKey = NanNew<v8::String>("groups");
-
-		// TODO: add type checking and dev-helpful error throwing
-
-		
-		if(msg->Has(groupKey)) {
-
-
+		if(msg->Has(groupKey))
+		{
 			Local<v8::Array> groups = Local<v8::Array>::Cast(msg->Get(groupKey));
-
-			for(int i = 0; i < (int) groups->Length(); i++) {
-
-				
+			for(int i=0; i<(int)groups->Length(); i++) {
 				Local<v8::Object> groupObj = groups->Get(i)->ToObject();
+				FIX::Group* group = new FIX::Group(
+						groupObj->Get(NanNew<v8::String>("index"))->ToInteger()->Value(),
+						groupObj->Get(NanNew<v8::String>("delim"))->ToInteger()->Value());
 
-				Local<v8::String> delimKey = NanNew<v8::String>("delim");
-				Local<v8::String> indexKey = NanNew<v8::String>("index");
-
-				if( ! groupObj->Has(indexKey)) {
-						NanThrowError("no index property found on object");
-				}
-				
-				if( ! groupObj->Has(delimKey)) {
-						NanThrowError("no delim property found on object");
-				}
-
-	
-				Local<v8::String> entriesKey = NanNew<v8::String>("entries");
-
-				if( ! groupObj->Has(entriesKey)) {
-						NanThrowError("no entries property found on object");
-				}
-
-				Local<v8::Array> groupEntries = Local<v8::Array>::Cast(groupObj->Get(entriesKey));
-				
-				for (int j = 0; j < (int) groupEntries->Length(); j++) {
-
+				Local<v8::Array> groupEntries = Local<v8::Array>::Cast(groupObj->Get(NanNew<v8::String>("entries")));
+				for(int j=0; j<(int)groupEntries->Length(); j++) {
 					Local<v8::Object> entry = groupEntries->Get(j)->ToObject();
+					Local<v8::Array> entryTags = entry->GetPropertyNames();
+					for(int k=0; k<(int)entryTags->Length(); k++) {
+						Local<v8::String> prop = entryTags->Get(k)->ToString();
+						String::Utf8Value keyStr(prop->ToString());
 
-					Local<v8::String> tagKey = NanNew<v8::String>("tags");
-
-					if(entry->Has(groupKey) || entry->Has(tagKey)) {
-
-						FIX::Group* group = new FIX::Group(
-							groupObj->Get(indexKey)->ToInteger()->Value(),
-							groupObj->Get(delimKey)->ToInteger()->Value());
-				
-						addFixTags(group, entry);
-						addFixGroups(group, entry);
-
-						map->addGroup(group->field(), *group);
-				
-					} else {
-
-						FIX::Group* group = new FIX::Group(
-							groupObj->Get(indexKey)->ToInteger()->Value(),
-							groupObj->Get(delimKey)->ToInteger()->Value());
-
-						// compat for old, non-nested format
-				
-						Local<v8::Array> entryTags = entry->GetPropertyNames();
-				
-						for(int k=0; k < (int) entryTags->Length(); k++) {
-				
-							Local<v8::String> prop = entryTags->Get(k)->ToString();
-							String::Utf8Value keyStr(prop->ToString());
-							String::Utf8Value valueStr(entry->Get(prop)->ToString());
-						
-							group->setField(atoi(std::string(*keyStr).c_str()), std::string(*valueStr));
-				
-						}
-				
-						map->addGroup(group->field(), *group);
-
+						String::Utf8Value valueStr(entry->Get(prop)->ToString());
+						group->setField(atoi(std::string(*keyStr).c_str()), std::string(*valueStr));
 					}
+					message->addGroup(*group);
 				}
 			}
 		}
-	}
-
-	static void addFixTrailer(FIX::Message* message, Local<v8::Object> msg) {
-
-		FIX::Trailer &msgTrailer = message->getTrailer();
 
 		Local<v8::String> trailerKey = NanNew<v8::String>("trailer");
-		
-		if(msg->Has(trailerKey)) {
-		
+		if(msg->Has(trailerKey))
+		{
 			Local<v8::Object> trailer = Local<v8::Object>::Cast(msg->Get(trailerKey));
 			Local<v8::Array> trailerTags = trailer->GetPropertyNames();
-		
 			for(int i=0; i<(int)trailerTags->Length(); i++) {
-		
 				Local<v8::String> prop = trailerTags->Get(i)->ToString();
 				String::Utf8Value keyStr(prop->ToString());
 
 				String::Utf8Value valueStr(trailer->Get(prop)->ToString());
 
 				msgTrailer.setField(atoi(std::string(*keyStr).c_str()), std::string(*valueStr));
-		
 			}
-		
 		}
-
 	}
 
-	static void js2Fix(FIX::Message* message, Local<v8::Object> msg) {
-
-		addFixHeader(message, msg);
-
-		addFixTags(message, msg);
-		
-		addFixGroups(message, msg);
-
-		addFixTrailer(message, msg);
-
-	}
-
-	static void addJsHeader(Local<v8::Object> msg, const FIX::Message* message) {
+	static void fix2Js(Local<v8::Object> msg, const FIX::Message* message) {
 		Local<v8::Object> header = NanNew<v8::Object>();
+		Local<v8::Object> tags = NanNew<v8::Object>();
+		Local<v8::Object> trailer = NanNew<v8::Object>();
+		Local<v8::Object> groups = NanNew<v8::Object>();
+
 		FIX::Header messageHeader = message->getHeader();
+		FIX::Trailer messageTrailer = message->getTrailer();
 
 		for(FIX::FieldMap::iterator it = messageHeader.begin(); it != messageHeader.end(); ++it)
 		{
 			header->Set(NanNew<Integer>(it->first), NanNew<v8::String>(it->second.getString().c_str()));
 		}
 
-		msg->Set(NanNew<v8::String>("header"), header);
-	}	
-
-	static void addJsTags(Local<v8::Object> msg, const FIX::FieldMap* map) {
-		Local<v8::Object> tags = NanNew<v8::Object>();
-		int noTags = 0;
-
-		for(FIX::FieldMap::iterator it = map->begin(); it != map->end(); ++it)
+		for(FIX::FieldMap::iterator it = message->begin(); it != message->end(); ++it)
 		{
 			tags->Set(NanNew<Integer>(it->first), NanNew<v8::String>(it->second.getString().c_str()));
-			noTags++;
 		}
-
-		if (noTags > 0) {
-			msg->Set(NanNew<v8::String>("tags"), tags);
-		}
-	}	
-
-	static void addJsTrailer(Local<v8::Object> msg, const FIX::Message* message) {
-		Local<v8::Object> trailer = NanNew<v8::Object>();
-		FIX::Trailer messageTrailer = message->getTrailer();
 
 		for(FIX::FieldMap::iterator it = messageTrailer.begin(); it != messageTrailer.end(); ++it)
 		{
 			trailer->Set(NanNew<Integer>(it->first), NanNew<v8::String>(it->second.getString().c_str()));
 		}
 
-		msg->Set(NanNew<v8::String>("trailer"), trailer);
-	}	
-
-	static void addJsGroups(Local<v8::Object> msg, const FIX::FieldMap* map) {
-		Local<v8::Object> groups = NanNew<v8::Object>();
-		int noGroups = 0;
-
-		for(FIX::FieldMap::g_iterator it = map->g_begin(); it != map->g_end(); ++it) {
+		for(FIX::FieldMap::g_iterator it = message->g_begin(); it != message->g_end(); ++it)
+		{
 			std::vector< FIX::FieldMap* > groupVector = it->second;
 			Handle<v8::Array> groupList = NanNew<v8::Array>(groupVector.size());
-			int i = 0;
-
-			for(std::vector< FIX::FieldMap* >::iterator v_it = groupVector.begin(); v_it != groupVector.end(); ++v_it) {
+			int i=0;
+			for(std::vector< FIX::FieldMap* >::iterator v_it = groupVector.begin(); v_it != groupVector.end(); ++v_it)
+			{
 				Handle<v8::Object> groupEntry = NanNew<v8::Object>();
-
 				FIX::FieldMap* fields = *v_it;
-
-				addJsTags(groupEntry, fields);
-
-				addJsGroups(groupEntry, fields);
-
+				for(FIX::FieldMap::iterator field_it = fields->begin(); field_it != fields->end(); ++field_it)
+				{
+					groupEntry->Set(NanNew<Integer>(field_it->first), NanNew<v8::String>(field_it->second.getString().c_str()));
+				}
 				groupList->Set(i, groupEntry);
-
 				i++;
 			}
 
 			groups->Set(NanNew<Integer>(it->first), groupList);
-
-			noGroups++;
 		}
 
-		if (noGroups > 0) {
-			msg->Set(NanNew<v8::String>("groups"), groups);
-		}
-	}	
-
-	static void fix2Js(Local<v8::Object> msg, const FIX::Message* message) {
-		
-		addJsHeader(msg, message);
-
-		addJsTags(msg, message);
-
-		addJsTrailer(msg, message);
-
-		addJsGroups(msg, message);
-		
+		msg->Set(NanNew<v8::String>("header"), header);
+		msg->Set(NanNew<v8::String>("tags"), tags);
+		msg->Set(NanNew<v8::String>("trailer"), trailer);
+		msg->Set(NanNew<v8::String>("groups"), groups);
 	}
 
 	static Local<Value> sessionIdToJs(const FIX::SessionID* sessionId) {
--- test/index.js
+++ test/index.js
@@ -3,314 +3,158 @@ var path = require('path');
 var quickfix = require('../');
 var should = require('should');
 
-var segfaultHandler = require('segfault-handler');
-
-segfaultHandler.registerHandler();
-
 var acceptorPropertiesFile = path.resolve(path.join(__dirname, 'support', 'acceptor.properties'));
 var initiatorPropertiesFile = path.resolve(path.join(__dirname, 'support', 'initiator.properties'));
 
-describe('initiator', function () {
+describe('node-quickfix', function () {
 
-  this.timeout(5000);
-
-  it('should throw if not supplied options', function () {
-    (function () { quickfix.initiator(); }).should.throw('FixInitiator requires an options parameter');
-  });
-
-  it('should throw if supplied options with no settings or propertiesFile', function () {
-    (function () { quickfix.initiator({}, {}); }).should.throw('you must provide FixInitiator either an options.settings string or options.propertiesFile path to a properties file');
-  });
-
-  it('should create a new initiator instance with fix beginString, senderCompID, targetCompID on defined session', function () {
-    var initiator = new quickfix.initiator({}, {
-      propertiesFile: initiatorPropertiesFile
-    });
-    initiator.should.not.eql(null);
-    initiator.isLoggedOn().should.eql(false);
-    initiator.getSessions()[0].beginString.should.eql('FIX.4.4');
-    initiator.getSessions()[0].senderCompID.should.eql('SenderCompID');
-    initiator.getSessions()[0].targetCompID.should.eql('TargetCompID');
-  });
-
-  it('should start and stop when called', function (done) {
-    var initiator = new quickfix.initiator({}, {
-      propertiesFile: initiatorPropertiesFile
-    });
-    initiator.start(function () {
-      // todo: started with local property
-      initiator.stop(function () {
-        // todo: started with local property
-        done();  
-      });
-    });
-  });
-
-});
-
-describe('acceptor', function () {
-
-  this.timeout(5000);
-
-  it('should throw if not supplied options', function () {
-    (function () { quickfix.acceptor(); }).should.throw('FixAcceptor requires an options parameter');
-  });
-
-  it('should throw if supplied options with no settings or propertiesFile', function () {
-    (function () { quickfix.acceptor({}, {}); }).should.throw('you must provide FixAcceptor either an options.settings string or options.propertiesFile path to a properties file');
-  });
-
-  it('should create a new acceptor instance with fix beginString, senderCompID, targetCompID on defined session', function () {
-    var acceptor = new quickfix.acceptor({}, {
-      propertiesFile: acceptorPropertiesFile
-    });
-
-    acceptor.should.not.eql(null);
-    acceptor.getSessions()[0].beginString.should.eql('FIX.4.4');
-    acceptor.getSessions()[0].senderCompID.should.eql('TargetCompID');
-    acceptor.getSessions()[0].targetCompID.should.eql('SenderCompID');
-  });
-
-  it('should start and stop when called', function (done) {
-    var acceptor = new quickfix.acceptor({}, {
-      propertiesFile: acceptorPropertiesFile
-    });
-    acceptor.start(function () {
-      // todo: started with local property
-      acceptor.stop(function () {
-        // todo: started with local property
-        done();  
-      });
-    });
-  });
-
-});
-
-describe('initiator + acceptor', function () {
-
-  this.timeout(5000);
-
-  var acceptorEmitter = new events.EventEmitter();
-  var initiatorEmitter = new events.EventEmitter();
- 
-  var acceptor = new quickfix.acceptor({
-    onCreate: acceptorEmitter.emit.bind(acceptorEmitter, 'onCreate'),
-    onLogon: acceptorEmitter.emit.bind(acceptorEmitter, 'onLogon'),
-    onLogout: acceptorEmitter.emit.bind(acceptorEmitter, 'onLogout'),
-    onLogonAttempt: acceptorEmitter.emit.bind(acceptorEmitter, 'onLogonAttempt'),
-    toAdmin: acceptorEmitter.emit.bind(acceptorEmitter, 'toAdmin'),
-    fromAdmin: acceptorEmitter.emit.bind(acceptorEmitter, 'fromAdmin'),
-    toApp: acceptorEmitter.emit.bind(acceptorEmitter, 'toApp'),
-    fromApp: acceptorEmitter.emit.bind(acceptorEmitter, 'fromApp'),
-  }, {
-    propertiesFile: acceptorPropertiesFile
-  });
- 
-  var initiator = new quickfix.initiator({
-    onCreate: initiatorEmitter.emit.bind(initiatorEmitter, 'onCreate'),
-    onLogon: initiatorEmitter.emit.bind(initiatorEmitter, 'onLogon'),
-    onLogout: initiatorEmitter.emit.bind(initiatorEmitter, 'onLogout'),
-    onLogonAttempt: initiatorEmitter.emit.bind(initiatorEmitter, 'onLogonAttempt'),
-    toAdmin: initiatorEmitter.emit.bind(initiatorEmitter, 'toAdmin'),
-    fromAdmin: initiatorEmitter.emit.bind(initiatorEmitter, 'fromAdmin'),
-    toApp: initiatorEmitter.emit.bind(initiatorEmitter, 'toApp'),
-    fromApp: initiatorEmitter.emit.bind(initiatorEmitter, 'fromApp'),
-  }, {
-    propertiesFile: initiatorPropertiesFile
-  });
-
-  var results = {};
+  describe('initiator', function () {
   
-  function setResult (result) {
-    results[result] = true;
-  }
-
-  [ 'onCreate',
-    'onLogon',
-    'onLogout',
-    'onLogonAttempt',
-    'toAdmin',
-    'fromAdmin',
-    'toApp',
-    'fromApp'].forEach(function (e) {
-    acceptorEmitter.on(e, setResult.bind(null, 'acceptor.' + e));
-    initiatorEmitter.on(e, setResult.bind(null, 'initiator.' + e));
-  });
- 
-  after(function (done) {
-    initiator.stop(function () {
-      acceptor.stop(done);
-    });
-  });
- 
-  before(function (done) {
-    acceptor.start(function () {
-      initiator.start(done);
-    });
-  });
-
-  it('should logon and negotiate connection', function (done) {
-    // TODO: come up with event to hook off off for this, instead of timeout
-    setTimeout(function () {
-      results['initiator.toAdmin'].should.eql(true);
-      results['acceptor.fromAdmin'].should.eql(true);
-      results['acceptor.toAdmin'].should.eql(true);
-      results['acceptor.onLogon'].should.eql(true);
-      results['initiator.fromAdmin'].should.eql(true);
-      results['initiator.onLogon'].should.eql(true);
-      done();
-    }, 1000);
-  });
-
-  it('should convert nested groups from json to fix and back', function (done) {
-
-    var message = {
-      header: {
-        8: 'FIXT.1.1',
-        35: 'CM',
-        49: 'SenderCompID2',
-        56: 'TargetCompID2'
-      },
-      tags: {
-        1511: 0,
-        1512: 2,
-        1666: '6ca92854-a64c-453c-9d8f-cfd2c7d643d9',
-        1667: '6ca92854-a64c-453c-9d8f-cfd2c7d643d9',
-        1760: 1
-      },
-      groups: [
-        {
-          index: 1677,
-          delim: 1671,
-          entries: [{
-            groups: [{
-              index: 1671,
-              delim: 1691,
-              entries: [{
-                tags: {
-                  1691: '650000089X',
-                  1692: 'D',
-                  1693: 3
-                }
-              }]
-            }],
-          }, {
-            groups: [{
-              index: 1671,
-              delim: 1691,
-              entries: [{
-                tags: {
-                  1691: '660000022X',
-                  1692: 'D',
-                  1693: 3
-                }
-              }]
-            }]
-          }]
-        }
-      ]
-    };
-
-    acceptorEmitter.once('fromApp', function (message) {
-      
-      message.header['8'].should.equal('FIXT.1.1');
-      message.header['35'].should.equal('CM');
-      message.header['49'].should.equal('SenderCompID2');
-      message.header['56'].should.equal('TargetCompID2');
-      message.tags['1511'].should.equal('0');
-      message.tags['1512'].should.equal('2');
-      message.tags['1666'].should.equal('6ca92854-a64c-453c-9d8f-cfd2c7d643d9');
-      message.tags['1667'].should.equal('6ca92854-a64c-453c-9d8f-cfd2c7d643d9');
-      message.tags['1677'].should.equal('2');
-      message.tags['1760'].should.equal('1');
-      message.groups['1677'].should.have.property('length', 2);
-      message.groups['1677'][0].tags.should.have.property('1671', '1');
-      message.groups['1677'][0].groups['1671'].should.have.property('length', 1);
-      message.groups['1677'][0].groups['1671'][0].tags.should.have.property('1691', '650000089X');
-      message.groups['1677'][0].groups['1671'][0].tags.should.have.property('1692', 'D');
-      message.groups['1677'][0].groups['1671'][0].tags.should.have.property('1693', '3');
-      message.groups['1677'][1].tags.should.have.property('1671', '1');
-      message.groups['1677'][1].groups['1671'].should.have.property('length', 1);
-      message.groups['1677'][1].groups['1671'][0].tags.should.have.property('1691', '660000022X');
-      message.groups['1677'][1].groups['1671'][0].tags.should.have.property('1692', 'D');
-      message.groups['1677'][1].groups['1671'][0].tags.should.have.property('1693', '3');
-      
-      done();
+    it('should throw if not supplied options', function () {
+      (function () { quickfix.initiator(); }).should.throw('FixInitiator requires an options parameter');
     });
 
-    initiator.send(message);
-
-  });
-
-  it('should convert non-nested groups from json to fix and back', function (done) {
-
-    var message = {
-      header: {
-        8: 'FIXT.1.1',
-        35: 'CM',
-        49: 'SenderCompID2',
-        56: 'TargetCompID2'
-      },
-      tags: {
-        1511: 0,
-        1512: 2,
-        1666: '6ca92854-a64c-453c-9d8f-cfd2c7d643d9',
-        1667: '6ca92854-a64c-453c-9d8f-cfd2c7d643d9',
-        1760: 1
-      },
-      groups: [
-        {
-          'index': 1677,
-          'delim': 1671,
-          'entries': [{
-              1671: 1,
-              1691: '650000089X',
-              1692: 'D',
-              1693: 3,
-              // 1669: 1,
-              // 1529: 1,
-              // 1530: 1,
-              // 1531: 200000000,
-              // 1766: 15000000,
-              // 1532: 'USD',
-              // 1670: 123,
-              // 2339: 3
-          }, {
-            1671: 1,
-            1691: '660000022X',
-            1692: 'D',
-            1693: 3,
-            // 1669: 1,
-            // 1529: 1,
-            // 1530: 1,
-            // 1531: 150000000,
-            // 1766: 5000000,
-            // 1532: 'USD',
-            // 1670: 456,
-            // 2339: 3
-          }]
-        }
-      ]
-    };
-
-    acceptorEmitter.once('fromApp', function (message) {
-
-      message.header['8'].should.equal('FIXT.1.1');
-      // message.header['9'].should.equal('256');
-      message.header['35'].should.equal('CM');
-      message.header['49'].should.equal('SenderCompID2');
-      message.header['56'].should.equal('TargetCompID2');
-      message.tags['1511'].should.equal('0');
-      message.tags['1512'].should.equal('2');
-      message.tags['1666'].should.equal('6ca92854-a64c-453c-9d8f-cfd2c7d643d9');
-      message.tags['1667'].should.equal('6ca92854-a64c-453c-9d8f-cfd2c7d643d9');
-      message.tags['1677'].should.equal('2');
-      message.tags['1760'].should.equal('1');
-      
-      done();
+    it('should throw if supplied options with no settings or propertiesFile', function () {
+      (function () { quickfix.initiator({}, {}); }).should.throw('you must provide FixInitiator either an options.settings string or options.propertiesFile path to a properties file');
     });
 
-    initiator.send(message);
+    it('should create a new initiator instance with fix beginString, senderCompID, targetCompID on defined session', function () {
+      var initiator = new quickfix.initiator({}, {
+        propertiesFile: initiatorPropertiesFile
+      });
+      initiator.should.not.eql(null);
+      initiator.isLoggedOn().should.eql(false);
+      initiator.getSessions()[0].beginString.should.eql('FIX.4.4');
+      initiator.getSessions()[0].senderCompID.should.eql('NODEQUICKFIX');
+      initiator.getSessions()[0].targetCompID.should.eql('ELECTRONIFIE');
+    });
+
+    it('should start and stop when called', function (done) {
+      var initiator = new quickfix.initiator({}, {
+        propertiesFile: initiatorPropertiesFile
+      });
+      initiator.start(function () {
+        // todo: started with local property
+        initiator.stop(function () {
+          // todo: started with local property
+          done();  
+        });
+      });
+    });
 
   });
+
+  describe('acceptor', function () {
+  
+    it('should throw if not supplied options', function () {
+      (function () { quickfix.acceptor(); }).should.throw('FixAcceptor requires an options parameter');
+    });
+
+    it('should throw if supplied options with no settings or propertiesFile', function () {
+      (function () { quickfix.acceptor({}, {}); }).should.throw('you must provide FixAcceptor either an options.settings string or options.propertiesFile path to a properties file');
+    });
+
+    it('should create a new acceptor instance with fix beginString, senderCompID, targetCompID on defined session', function () {
+      var acceptor = new quickfix.acceptor({}, {
+        propertiesFile: acceptorPropertiesFile
+      });
+
+      acceptor.should.not.eql(null);
+      acceptor.getSessions()[0].beginString.should.eql('FIX.4.4');
+      acceptor.getSessions()[0].senderCompID.should.eql('ELECTRONIFIE');
+      acceptor.getSessions()[0].targetCompID.should.eql('NODEQUICKFIX');
+    });
+
+    it('should start and stop when called', function (done) {
+      var acceptor = new quickfix.acceptor({}, {
+        propertiesFile: acceptorPropertiesFile
+      });
+      acceptor.start(function () {
+        // todo: started with local property
+        acceptor.stop(function () {
+          // todo: started with local property
+          done();  
+        });
+      });
+    });
+
+  });
+
+  describe('inititator + acceptor', function () {
+
+    this.timeout(5000);
+
+    var acceptorEmitter = new events.EventEmitter();
+    var initiatorEmitter = new events.EventEmitter();
+   
+    var acceptor = new quickfix.acceptor({
+      onCreate: acceptorEmitter.emit.bind(acceptorEmitter, 'onCreate'),
+      onLogon: acceptorEmitter.emit.bind(acceptorEmitter, 'onLogon'),
+      onLogout: acceptorEmitter.emit.bind(acceptorEmitter, 'onLogout'),
+      onLogonAttempt: acceptorEmitter.emit.bind(acceptorEmitter, 'onLogonAttempt'),
+      toAdmin: acceptorEmitter.emit.bind(acceptorEmitter, 'toAdmin'),
+      fromAdmin: acceptorEmitter.emit.bind(acceptorEmitter, 'fromAdmin'),
+      toApp: acceptorEmitter.emit.bind(acceptorEmitter, 'toApp'),
+      fromApp: acceptorEmitter.emit.bind(acceptorEmitter, 'fromApp'),
+    }, {
+      propertiesFile: acceptorPropertiesFile
+    });
+   
+    var initiator = new quickfix.initiator({
+      onCreate: initiatorEmitter.emit.bind(initiatorEmitter, 'onCreate'),
+      onLogon: initiatorEmitter.emit.bind(initiatorEmitter, 'onLogon'),
+      onLogout: initiatorEmitter.emit.bind(initiatorEmitter, 'onLogout'),
+      onLogonAttempt: initiatorEmitter.emit.bind(initiatorEmitter, 'onLogonAttempt'),
+      toAdmin: initiatorEmitter.emit.bind(initiatorEmitter, 'toAdmin'),
+      fromAdmin: initiatorEmitter.emit.bind(initiatorEmitter, 'fromAdmin'),
+      toApp: initiatorEmitter.emit.bind(initiatorEmitter, 'toApp'),
+      fromApp: initiatorEmitter.emit.bind(initiatorEmitter, 'fromApp'),
+    }, {
+      propertiesFile: initiatorPropertiesFile
+    });
+
+    var results = {};
+    
+    function setResult (result) {
+      results[result] = true;
+    }
+
+    [ 'onCreate',
+      'onLogon',
+      'onLogout',
+      'onLogonAttempt',
+      'toAdmin',
+      'fromAdmin',
+      'toApp',
+      'fromApp'].forEach(function (e) {
+      acceptorEmitter.on(e, setResult.bind(null, 'acceptor.' + e));
+      initiatorEmitter.on(e, setResult.bind(null, 'initiator.' + e));
+    });
+   
+    after(function (done) {
+      initiator.stop(function () {
+        acceptor.stop(done);
+      });
+    });
+   
+    before(function (done) {
+      acceptor.start(function () {
+        initiator.start(done);
+      });
+    });
+
+    it('should logon and negotiate connection', function (done) {
+      // TODO: come up with event to hook off off for this, instead of timeout
+      setTimeout(function () {
+        results['initiator.toAdmin'].should.eql(true);
+        results['acceptor.fromAdmin'].should.eql(true);
+        results['acceptor.toAdmin'].should.eql(true);
+        results['acceptor.onLogon'].should.eql(true);
+        results['initiator.fromAdmin'].should.eql(true);
+        results['initiator.onLogon'].should.eql(true);
+        done();
+      }, 1000);
+    });
+  });
+
 });
